71746382f8b81360d7dbf7115fc6c9d7da3f067d
Carl Steele <casteeleu@gmail.com>
2018-08-06 23:41:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 function fetchBooks() {
-  //write fetch request to the Game of Thrones API
-} fetch('https://anapioficeandfire.com/api/books')
+ fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
   .then(renderBooks)
-   
+}
 
 
 function renderBooks(json) {
